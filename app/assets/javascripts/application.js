63fd5039da26f6f6179cdc71f195bb2277ca145a
Nicolás Lundin <nlundin@gmail.com>
2017-03-13 09:57:12 -0300

    BS4! + file cleaning at /assets/stylesheets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,7 +25,7 @@
 //= require bootstrap-image-gallery
 
 // # BOOTSTRAP
-//= require twbs/bootstrap
+//= require bootstrap-sprockets
 
 // # ALL THE REST
 // # require_tree .
@@ -36,7 +36,7 @@
 //= require autocomplete-rails
 
 var QueryString = function () {
-  // This function is anonymous, is executed immediately and 
+  // This function is anonymous, is executed immediately and
   // the return value is assigned to QueryString!
   var query_string = {};
   var query = window.location.search.substring(1);
@@ -54,11 +54,11 @@ var QueryString = function () {
     } else {
       query_string[pair[0]].push(decodeURIComponent(pair[1]));
     }
-  } 
+  }
     return query_string;
 }();
 
-// //OPEN MODAL AFTER CREATING A LOCATION 
+// //OPEN MODAL AFTER CREATING A LOCATION
 // if (QueryString.open=="newuser") {
 //   //open modal to register user
 //   $(window).load(function(){
@@ -68,4 +68,3 @@ var QueryString = function () {
 // }else if (QueryString.open=="share"){
 //   //open share modal
 // }
-
