6c18999f3f462587097bdb5765c17897d1d81a53
Juan Ignacio Cortes <jicortes2@uc.cl>
2017-05-29 10:35:18 -0400

    admin seed fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -77,9 +77,8 @@ admin.email="hello@mego.cl"
 admin.first_name=Faker::Name.first_name
 admin.last_name=Faker::Name.last_name
 admin.avatar_url=Faker::Avatar.image
-#admin.owning=true
-#admin.tenant=true
-admin.status = 'admin'
+admin.owner=true
+admin.tenant=true
 admin.save
 admin.update_attribute(:status,"admin")
 # approves 16 locations
