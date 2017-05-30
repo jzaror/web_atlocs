94bb3de6c8c18b2049572ceb921ef11aef688be5
Juan Ignacio Cortes <jicortes2@uc.cl>
2017-05-30 17:52:53 -0400

    confirmation works

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170525204842) do
+ActiveRecord::Schema.define(version: 20170530214116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -179,7 +179,7 @@ ActiveRecord::Schema.define(version: 20170525204842) do
     t.string   "deposit_bank"
     t.string   "deposit_account"
     t.datetime "deleted_at"
-    t.boolean  "owner"
+    t.boolean  "owning"
     t.boolean  "tenant"
   end
 
