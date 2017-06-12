33a5846c71bf85fb0c844362206df9b610eb0fcb
ZSeba <sebastian.vidalm@gmail.com>
2017-06-12 02:06:45 -0400

    more cleanup

--- db/schema.rb
+++ db/schema.rb
@@ -11,27 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< 6bd090a200a12a4e304a547ac4dd17cc4e879b63
-<<<<<<< 8d0a528701c151a015109b9ad84edb4ae96a3230
-<<<<<<< 8bebd5ffb0f6c3f1703353d75765ee179b9dd9eb
-<<<<<<< dba39cc1b4bba08e4221c9caeb781b2d08e1766c
-<<<<<<< aa53afb8d0f5b3571eecf475e1a4cd6c28701289
-ActiveRecord::Schema.define(version: 20170530215804) do
-=======
-ActiveRecord::Schema.define(version: 20170531175955) do
->>>>>>> fix mailer preview
-=======
-ActiveRecord::Schema.define(version: 20170530215804) do
->>>>>>> mail text booking and payment
-=======
-ActiveRecord::Schema.define(version: 20170531175955) do
->>>>>>> welcome mail diferent users
-=======
-ActiveRecord::Schema.define(version: 20170602230416) do
->>>>>>> verify email accounts done
-=======
 ActiveRecord::Schema.define(version: 20170606231342) do
->>>>>>> added other comments
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -201,19 +181,7 @@ ActiveRecord::Schema.define(version: 20170606231342) do
     t.string   "deposit_bank"
     t.string   "deposit_account"
     t.datetime "deleted_at"
-<<<<<<< 8bebd5ffb0f6c3f1703353d75765ee179b9dd9eb
-<<<<<<< dba39cc1b4bba08e4221c9caeb781b2d08e1766c
-<<<<<<< aa53afb8d0f5b3571eecf475e1a4cd6c28701289
-    t.boolean  "owner"
-=======
     t.boolean  "owning"
->>>>>>> fix mailer preview
-=======
-    t.boolean  "owner"
->>>>>>> mail text booking and payment
-=======
-    t.boolean  "owning"
->>>>>>> welcome mail diferent users
     t.boolean  "tenant"
     t.string   "provider"
     t.string   "uid"
