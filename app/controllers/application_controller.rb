6fff1af3efb145ec1b5e847402c3c70a9aa9cb5d
ZSeba <sebastian.vidalm@gmail.com>
2017-07-21 00:20:06 -0400

    added backend config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :store_location
+  before_action :set_locale
 
   private
-
 
   def store_location
     session[:requestUri] = params[:requestUri] if params[:requestUri].present?
@@ -13,6 +13,14 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
   end
 
   rescue_from CanCan::AccessDenied do |exception|
